a837528de31963c00481cac4b48271130216e78e
sereja-s <serejasuvorov1979@gmail.com>
2026-06-09 11:46:58 +0300

    настройка сonfig для dev, -лишние темы и плагины

--- wp-config.php
+++ wp-config.php
@@ -86,10 +86,28 @@ $table_prefix = 'wp_';
  *
  * @link https://developer.wordpress.org/advanced-administration/debug/debug-wordpress/
  */
-define('WP_DEBUG', false);
+//define('WP_DEBUG', false);
+
+define('WP_DEBUG', true);
+define('WP_DEBUG_LOG', true);
+define('WP_DEBUG_DISPLAY', false);
 
 /* Add any custom values between this line and the "stop editing" line. */
 
+/**
+ * Отключить встроенный редактор
+ */
+define('DISALLOW_FILE_EDIT', true);
+
+/**
+ * Отключить автоматические обновления WordPress.
+ */
+define('AUTOMATIC_UPDATER_DISABLED', true);
+
+/**
+ * Отключить автоматические обновления ядра WordPress.
+ */
+define('WP_AUTO_UPDATE_CORE', false);
 
 
 /* That's all, stop editing! Happy publishing. */
